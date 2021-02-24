80126cfa532848d364c17e0a506661172c73aac4
Jan Hajic Jr <hajicj@ufal.mff.cuni.cz>
2021-02-24 12:47:44 +0100

    debugging docker deploy: npm audit fix was returning 1

--- ommr4all-deploy/deploy/run_deploy.py
+++ ommr4all-deploy/deploy/run_deploy.py
@@ -33,7 +33,7 @@ def main():
     os.chdir('modules/ommr4all-client')
     check_call(['sed', '-i', '-e', 's#routerLink="/imprint"#href="https://www.uni-wuerzburg.de/en/sonstiges/imprint-privacy-policy/"#g', 'src/app/app.component.html'])
     check_call(['npm', 'install'])
-    check_call(['npm', 'audit', 'fix'])
+    check_call(['npm', 'audit', 'fix', '--audit-level', 'high'])
     for config in ['production', 'production-de']:
         check_call(['ng', 'build', '--configuration', config])
 
